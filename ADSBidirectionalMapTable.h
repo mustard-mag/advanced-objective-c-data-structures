b4a999bf0f8a600e4fd0727514b6812fb9afd183
Richard Stelling <richard.stelling@vistair.com>
2013-12-09 14:14:46 +0000

    - Stubs for Bidirectional Map Table object.

---
+++ ADSBidirectionalMapTable.h
@@ -0,0 +1,13 @@
+//
+//  ADSBidirectionalMapTable.h
+//
+//
+//  Created by Richard Stelling on 09/12/2013.
+//  Copyright (c) 2013 Empirical Magic Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADSBidirectionalMapTable : NSObject
+
+@end
---
+++ ADSBidirectionalMapTable.m
@@ -0,0 +1,13 @@
+//
+//  ADSBidirectionalMapTable.m
+//  
+//
+//  Created by Richard Stelling on 09/12/2013.
+//  Copyright (c) 2013 Empirical Magic Ltd. All rights reserved.
+//
+
+#import "ADSBidirectionalMapTable.h"
+
+@implementation ADSBidirectionalMapTable
+
+@end
