1a5c0e54a7ea165dd1cea363e1b60d98d7e6ef5a
Richard Stelling <richard.stelling@vistair.com>
2014-01-09 16:09:49 +0000

    - Stubbed out linked list data structure methods

--- ADSDoublyLinkedList.m
+++ ADSDoublyLinkedList.m
@@ -10,4 +10,55 @@
 
 @implementation ADSDoublyLinkedList
 
+#pragma mark - Taverse List
+
+- (void)forward
+{
+    
+}
+
+///Move backward in list
+- (void)backward
+{
+    
+}
+
+#pragma mark - Adding/Removing Nodes
+
+///Add an object to the list. This object will be connected to head.
+- (void)add:(id)anObject
+{
+    
+}
+
+///Remove the current object at index and connect (index - 1) to (index + 1)
+- (void)remove
+{
+    
+}
+
+///Remove all nodes forward of index, index will become head
+- (void)trimForward
+{
+    
+}
+
+///Remove all nodes behind index, index will become tail
+- (void)trimBackward
+{
+    
+}
+
+///Trim both forwards and backwards leaving only the object at `index` e.g. NULL<->index<->NULL
+- (void)trimAll
+{
+    
+}
+
+///Remov all objects
+- (void)empty
+{
+    
+}
+
 @end
