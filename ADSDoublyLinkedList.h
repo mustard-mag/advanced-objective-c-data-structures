ae14cf8db207998ac790073f2a194c7641322a6a
Richard Stelling <richard.stelling@vistair.com>
2014-01-09 15:28:59 +0000

    - Stubbed out linked list data structure

---
+++ ADSDoublyLinkedList.h
@@ -0,0 +1,13 @@
+//
+//  ADSDoublyLinkedList.h
+//  advanced-objective-c-data-structures.git
+//
+//  Created by Richard Stelling on 09/01/2014.
+//  Copyright (c) 2014 Empirical Magic Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ADSDoublyLinkedList : NSObject
+
+@end
---
+++ ADSDoublyLinkedList.m
@@ -0,0 +1,13 @@
+//
+//  ADSDoublyLinkedList.m
+//  advanced-objective-c-data-structures.git
+//
+//  Created by Richard Stelling on 09/01/2014.
+//  Copyright (c) 2014 Empirical Magic Ltd. All rights reserved.
+//
+
+#import "ADSDoublyLinkedList.h"
+
+@implementation ADSDoublyLinkedList
+
+@end
